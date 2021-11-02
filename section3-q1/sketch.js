95c7ef815ef85388c75bbcb05c092786a193f33c
Yotaro-Kitahara <92347055+Yotaro-Kitahara@users.noreply.github.com>
2021-11-02 11:23:47 +0900

    change section3-q1 new

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,35 +1,31 @@
-// スプラ〇ゥーンのような塗りつぶして自分の陣地を広げる2人対戦ゲームをイメージして作りました。
+// 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
+let count;
+let cycle;
+
 
 function setup(){
-  createCanvas(windowWidth, windowHeight);
-  x=width*3/4 ;
-  y=height/2;
-  a=width/4;
-  b=height/2;
-
+ createCanvas(200, 200);
+ count = 0;
+ cycle = 100;
 
 }
 
 function draw(){
-
-{circle(x, y, 100);
-  fill(255,100,100);
-  if(keyIsDown(UP_ARROW)){y-=5;}
-  if(keyIsDown(DOWN_ARROW)){y+=5;}
-  if(keyIsDown(LEFT_ARROW)){x-=5;}
-  if(keyIsDown(RIGHT_ARROW)) {x+=5;}
-  x=(x+width)%width;
-  y=(y+height)%height;
+ background(160, 192, 255);
+ if(keyIsDown("A".charCodeAt(0))){
+ count=(count+5)%cycle;
 }
-
-{circle(a, b, 100);
-  fill(100,100,255);
-if(keyIsDown("W".charCodeAt(0))){b-=5;}
-if(keyIsDown("S".charCodeAt(0))){b+=5;}
-if(keyIsDown("A".charCodeAt(0))){a-=5;}
-if(keyIsDown("D".charCodeAt(0))){a+=5;}
-a=(a+width)%width;
-b=(b+height)%height;
-
+else {
+  count=(count+3)%cycle
 }
+ // BLANK[1]
+
+if(count<50){
+  ellipse(width/2,height/2,count)
 }
+ else {
+   ellipse(width/2,height/2,100-count)
+
+ }
+
+ }
